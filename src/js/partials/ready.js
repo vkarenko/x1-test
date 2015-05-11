1632b375f5fbac027022184774e8fd0addfdc1b5
vkarenko <vitalii.karenko@me.com>
2015-05-11 23:27:17 +0300

    not tested for IE

---
+++ src/js/partials/ready.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.mSlider').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		autoplay: true,
+		speed: 1000,
+		autoplaySpeed: 3000,
+		fade: true,
+		cssEase: 'linear'
+	});
+});
