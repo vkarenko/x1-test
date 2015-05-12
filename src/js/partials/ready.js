0f82c70dfe4af965779b22b6ecae2321dd737210
vkarenko <vitalii.karenko@me.com>
2015-05-12 13:47:51 +0300

    fixes for IE & responsive menu

--- src/js/partials/ready.js
+++ src/js/partials/ready.js
@@ -9,4 +9,27 @@ $(function(){
 		fade: true,
 		cssEase: 'linear'
 	});
+
+	$('.h-nav-icon').click(function (){
+		$(this).next().toggle();
+	})
+
+	function GetIEVersion() {
+		var sAgent = window.navigator.userAgent;
+		var Idx = sAgent.indexOf("MSIE");
+
+		// If IE, return version number.
+		if (Idx > 0) 
+			return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+		// If IE 11 then look for Updated user agent string.
+		else if (!!navigator.userAgent.match(/Trident\/7\./)) 
+			return 11;
+
+		else
+			return 0; //It is not IE
+	}
+
+	if (GetIEVersion() > 0) 
+	$('body').addClass('IE'+GetIEVersion());
 });
