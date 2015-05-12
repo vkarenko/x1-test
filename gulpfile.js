8231a4ba8eb0dbb48f803f091a3c2e6631bf599c
vkarenko <vitalii.karenko@me.com>
2015-05-12 14:36:38 +0300

    uglify turnOn for JS compilation

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ gulp.task('js:build', function () {
 	gulp.src(path.src.js) 
 		.pipe(rigger()) 
 		.pipe(sourcemaps.init()) 
-		//.pipe(uglify()) 
+		.pipe(uglify()) 
 		.pipe(sourcemaps.write()) 
 		.pipe(gulp.dest(path.build.js))
 		.pipe(reload({stream: true}));
